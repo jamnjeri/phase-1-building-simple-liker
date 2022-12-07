7a6d01154dd18a95e4d01d6de984150c626cabb1
Jamila <jamnjeri@gmail.com>
2022-12-07 12:36:34 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const colorStates = 'red'
 
 // Your JavaScript code goes here!
 
+//Grab the heart buttons 
+let hearts = document.querySelectorAll(".like-glyph")
+
+//Add events to them
+function likeCallBack(e){
+  const heart = e.target;
+  mimicServerCall()
+  .then(function(serverMessage){
+    //Send an alert if sending of the data is successful
+    alert("You notified the server!");
+    alert(serverMessage);
+    //Makes it a full heart
+    heart.innerText = FULL_HEART;
+    //Makes the heart red
+    heart.style.color = colorStates;
+  })
+  .catch(function(error){
+    alert("Something went wrong!");
+  });
+}
+
+for (const addHearts of hearts)(
+  addHearts.addEventListener("click", likeCallBack)
+)
 
 
 
